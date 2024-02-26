fe385c2accb05f0791843e3296fe965dc0c53b7e
RedminePower <redmine.power.com@gmail.com>
2024-02-26 13:12:27 +0900

    Fix a bug prevented starting with Redmine 5.x

--- lib/redmine_git_hook/plugin.rb
+++
@@ -1,8 +0,0 @@
-module RedmineGitHook
-  # Run the classic redmine plugin initializer after rails boot
-  class Plugin < ::Rails::Engine
-    config.after_initialize do
-      require File.expand_path("../../../init", __FILE__)
-    end
-  end
-end
